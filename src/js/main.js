680fdea076e5fb809acebda0d57084967afc5b75
Serhii Haber <haberserhii@icloud.com>
2023-09-14 17:16:45 +0300

    hero.upd

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,14 @@
-import * as basicLightbox from 'basiclightbox'
+import * as basicLightbox from 'basiclightbox';
 // import "basicLightbox/dist/basicLightbox.min.css" Не можу підключити стилі
 import Notiflix from 'notiflix';
-import axios from "axios";
+import axios from 'axios';
 import SlimSelect from 'slim-select';
 // import "slim-select/dist/slimselect.css"; Не можу підключити стилі
 import Swiper from 'swiper';
 import 'swiper/css';
 
 // SERVICE-API
-import { } from "./partials-js/service-api"
+import {} from './partials-js/service-api';
 
 // MENU-MODAL
-import { } from "./partials-js/menu"
+import {} from './partials-js/menu';
