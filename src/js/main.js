9b1bc89fd58cb4a34e5210e03593830a6653a0e9
daniil0040 <danyapolishchuk777@gmail.com>
2023-09-14 00:11:39 -0500

    prepearing librarys

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,11 @@
-
+import * as basicLightbox from 'basiclightbox'
+import "basicLightbox/dist/basicLightbox.min.css"
+import Notiflix from 'notiflix';
+import axios from "axios";
+import SlimSelect from 'slim-select';
+// import "slim-select/dist/slimselect.css"; Не можу підключити стилі
+import Swiper from 'swiper';
+import 'swiper/css';
 
 // SERVICE-API
 import { } from "./partials-js/service-api"
