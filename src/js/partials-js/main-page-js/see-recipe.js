80d3c62617f589145fbd40d0cde8d8b29cbc7563
havr1985 <gavrtel1985@gmail.com>
2023-09-15 23:17:33 +0300

    Update see-recipe.js

--- src/js/partials-js/main-page-js/see-recipe.js
+++ src/js/partials-js/main-page-js/see-recipe.js
@@ -1,15 +1,29 @@
 import * as basicLightbox from 'basiclightbox';
 import "basicLightbox/dist/basicLightbox.min.css";
+import { getRecipeById } from '../service-api';
 
-const seeRecipe = document.querySelector('.resipes-card');
+const seeRecipe = document.querySelector('.cards-container-js');
 
 seeRecipe.addEventListener('click', onClickRecipe);
 
 
 function onClickRecipe(event) {
-    if (!event.target.classList.contains('btn-see-recipe')) {
+    if (!event.target.classList.contains('btn-see-recipe-js')) {
         return;
     };
 
-    
+    const currentCard = event.target.closest('.btn-see-recipe-js');
+    const currentCardId = currentCard.dataset.id;
+
+    getCard(currentCardId);
+
+};
+
+async function getCard(id) {
+    try {
+        const card = await getRecipeById(id);
+        console.log(card);
+    } catch (error) {
+        console.log(error);
+    }
 }
