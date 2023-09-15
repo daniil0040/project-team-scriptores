fa2c04491293c6096371d8e722321f2bb53dce1a
Vovcharenko Anna <129333985+AnnaVovcharenko@users.noreply.github.com>
2023-09-16 01:19:21 +0300

    11

--- src/js/partials-js/main-page-js/categories.js
+++ src/js/partials-js/main-page-js/categories.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
-import Notiflix from 'notiflix';
+import {getAllSomething} from "../service-api"
 
 
 
-const category_url = 'https://tasty-treats-backend.p.goit.global/api/categories';
-axios.get(category_url).then(response => {
-  const recipes = document.querySelector('.nav-scroller');
+//const category_url = 'https://tasty-treats-backend.p.goit.global/api/categories';
+getAllSomething('categories').then(response => {
+  const recipes = document.querySelector('.category-list');
   const recipes_markup = response.data
     .map(
       category => `<li>
@@ -16,3 +16,20 @@ axios.get(category_url).then(response => {
   recipes.innerHTML = recipes_markup;
 
 });
+
+
+
+/* 
+const category_url = 'https://tasty-treats-backend.p.goit.global/api/categories';
+axios.get(category_url).then(response => {
+  const recipes = document.querySelector('.category-list');
+  const recipes_markup = response.data
+    .map(
+      category => `<li>
+      <button class="category-button" href="#">${category.name}</button>
+      </li>`
+    )
+    .join('');
+  recipes.innerHTML = recipes_markup;
+
+});  */
