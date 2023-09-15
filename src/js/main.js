158f9ca9235afe37f6429054a787b6d8fc342a17
daniil0040 <danyapolishchuk777@gmail.com>
2023-09-15 16:16:55 -0500

    fixes

---
+++ src/js/main.js
@@ -0,0 +1,44 @@
+import * as basicLightbox from 'basiclightbox'
+// import "basicLightbox/dist/basicLightbox.min.css" 
+import Notiflix from 'notiflix';
+import axios from "axios";
+import SlimSelect from 'slim-select';
+// import "slim-select/dist/slimselect.css"; 
+import Swiper from 'swiper';
+import 'swiper/css';
+
+// SERVICE-API
+import { } from "./partials-js/service-api"
+
+// MENU-MODAL
+import "./partials-js/menu"
+
+// PARTIALS
+////////////////////////////
+
+// ORDER NOW
+import { } from "./partials-js/main-page-js/order-now"
+
+// SWIPER
+import { } from "./partials-js/main-page-js/swiper"
+
+// CATEGORIES
+import { } from "./partials-js/main-page-js/categories"
+
+// POPULAR
+import { } from "./partials-js/main-page-js/popular"
+
+// ALL CATEGORIES CARDS
+import { } from "./partials-js/main-page-js/all-categ-cards"
+
+// CATEGORIES CARDS
+import { } from "./partials-js/main-page-js/categ-cards"
+
+// SEARCH FILTER
+import { } from "./partials-js/main-page-js/search-filter"
+
+// SEE RECIPE
+import { } from "./partials-js/main-page-js/see-recipe"
+
+// PAGINATION
+import { } from "./partials-js/main-page-js/pagination"
