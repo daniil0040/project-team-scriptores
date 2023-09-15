9f52ed1e2f869ce6b45f0e636c3eea88007eb060
Oleh <oleh.tsarevych@gmail.com>
2023-09-15 22:22:07 +0300

    comit

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,15 @@
-import * as basicLightbox from 'basiclightbox'
-// import "basicLightbox/dist/basicLightbox.min.css" 
+import * as basicLightbox from 'basiclightbox';
+// import "basicLightbox/dist/basicLightbox.min.css"
 import Notiflix from 'notiflix';
-import axios from "axios";
+import axios from 'axios';
 import SlimSelect from 'slim-select';
-// import "slim-select/dist/slimselect.css"; 
+// import "slim-select/dist/slimselect.css";
 import Swiper from 'swiper';
 import 'swiper/css';
 
 // SERVICE-API
-import { } from "./partials-js/service-api"
+import {} from './partials-js/service-api';
 
 // MENU-MODAL
-import { } from "./partials-js/menu"
+import {} from './partials-js/menu';
+import './partials-js/select';
