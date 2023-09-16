f954160901868d081ff6b5e41e733674ed409e4a
Vovcharenko Anna <129333985+AnnaVovcharenko@users.noreply.github.com>
2023-09-16 12:50:53 +0300

    категорії

---
+++ src/js/partials-js/main-page-js/categories.js
@@ -0,0 +1,52 @@
+//import axios from 'axios';
+import { getAllSomething } from "../service-api"
+const recipes = document.querySelector('.category-list');
+//
+getAllSomething('categories').then(data => {
+    recipes.insertAdjacentHTML('beforeend', recipesMarkup(data))
+    function recipesMarkup() {
+        return data.map(({ _id, name }) =>
+            `<li>
+            <button id="${_id}" class="category-button">${name}</button>
+            </li>`
+        )
+            .join('');
+    }
+});
+
+
+
+
+//Додаткові методи(функції) які також працюють 	
+
+/*const category_url = 'https://tasty-treats-backend.p.goit.global/api/categories';
+axios.get(category_url).then(response => {
+    const recipes = document.querySelector('.category-list');
+    const recipes_markup = response.data
+      .map(
+        category => `<li>
+        <button class="category-button" href="#">${category.name}</button>
+        </li>`
+      )
+      .join('');
+    recipes.innerHTML = recipes_markup;
+  
+  });*/
+
+
+  /*
+getAllSomething("categories")
+    .then(data => {
+    recipes.insertAdjacentHTML('beforeend', recipesMarkup(data)) 
+})
+    .catch(err => {
+        console.log(err);
+    })
+
+function recipesMarkup(data) {
+    return data.map(({ _id, name }) =>
+         `<li>
+            <button id="${_id}" class="category-button">${name}</button>
+            </li>
+    ).join('');
+}*/
