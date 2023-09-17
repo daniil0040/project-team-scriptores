6d47edec5fe5764fdd131864071c1475def83f27
Ivan Nodyanosh <111501255+IvaNodyanosh@users.noreply.github.com>
2023-09-17 21:48:50 +0300

    corrected task

--- src/js/partials-js/main-page-js/all-categ-btn.js
+++ src/js/partials-js/main-page-js/all-categ-btn.js
@@ -1,25 +1,30 @@
-import { createAllCategCardsMarkup } from "./all-categ-cards"
-import { isAllCategories } from "./categories"
+import { createAllCategCardsMarkup } from "./all-categ-cards";
 import { getAllRecipes } from "../service-api";
-import { cardsContainer } from './all-categ-cards';
+import { cardsContainer } from './categories';
+import { resetPrev } from "./categories";
+
 export const buttonAllCategories = document.querySelector(".all-categories")
 
 
-
 const auditAllCateg = function () {
-    if (!isAllCategories || buttonAllCategories.classList.contains("all-categories-button-active")) {
+    if (buttonAllCategories.classList.contains("all-categories-button-active")) {
+        resetPrev();
+
+        buttonAllCategories.classList.remove("all-categories-button-active");
         getAllRecipes()
         .then(data => {
             console.log(data)
             cardsContainer.innerHTML = createAllCategCardsMarkup(data.results);
-            isAllCategories = true;
+            
+            
+            
        
         })
         .catch(error => {
             console.log(error);
         })
     } else {
-        buttonAllCategories.classList.add("all-categories-button-active");
+        
         return
     }
 }
