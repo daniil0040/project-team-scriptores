02ce8ac9e0c2c37f1ec09d75d117fc6ab0da7652
Ivan Nodyanosh <111501255+IvaNodyanosh@users.noreply.github.com>
2023-09-17 19:35:55 +0300

    created a function for the button "all-categories"

--- src/js/partials-js/main-page-js/all-categ-btn.js
+++ src/js/partials-js/main-page-js/all-categ-btn.js
@@ -1,2 +1,26 @@
 import { createAllCategCardsMarkup } from "./all-categ-cards"
+import { isAllCategories } from "./categories"
+import { getAllRecipes } from "../service-api";
+import { cardsContainer } from './categories';
+export const buttonAllCategories = document.querySelector(".all-categories")
+
+
+
+const auditAllCateg = function () {
+    if (!isAllCategories) {
+        getAllRecipes()
+        .then(data => {
+            console.log(data)
+            cardsContainer.innerHTML = createAllCategCardsMarkup(data.results);
+            isAllCategories = true;
+       
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    }return
+}
+
+buttonAllCategories.addEventListener('click', auditAllCateg);
+
 
