40574a44a7c6ba4ecf27310befde2abaf3f8f487
havr1985 <gavrtel1985@gmail.com>
2023-09-17 19:03:58 +0300

    modal window finish

--- src/js/partials-js/main-page-js/see-recipe.js
+++ src/js/partials-js/main-page-js/see-recipe.js
@@ -1,11 +1,13 @@
 import * as basicLightbox from 'basiclightbox';
 // import "basicLightbox/dist/basicLightbox.min.css";
 import { getRecipeById } from '../service-api';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const seeRecipe = document.querySelector('.cards-container-js');
+const seeRecipePopular = document.querySelector('.popular-recipes-container');
 
 seeRecipe.addEventListener('click', onClickRecipe);
-
+seeRecipePopular.addEventListener('click', onClickPopular);
 
 function onClickRecipe(event) {
     if (!event.target.classList.contains('btn-see-recipe-js')) {
@@ -16,39 +18,71 @@ function onClickRecipe(event) {
     const currentCardId = currentCard.dataset.id;
 
     getCard(currentCardId);
-
 };
+
+function onClickPopular(event) {
+    const currentPopularCard = event.target.closest('.popular-recipes-wrap');
+    const currentPopularCardId = currentPopularCard.dataset.id;
+
+    getCard(currentPopularCardId);
+}
 
 async function getCard(id) {
     try {
         const card = await getRecipeById(id);
-        console.log(card);
+        loadBasicLightbox(card);
+    } catch (error) {
+        console.log(error);
+    }
+};
 
-        const { thumb, title, rating, time, instructions, ingredients, tags, youtube, } = card;
-        const ingMark = ingredients.map(({ name, measure }) => `<li class="ing-item">
+function loadBasicLightbox(card) {
+    const instance = basicLightbox.create(createMurkUpModal(card));
+    instance.show();
+
+    const favorite = document.querySelector('.js-favorite-btn');
+    favorite.addEventListener('click', () => addFavorite(card));
+
+    const closeModal = document.querySelector('.js-modal-close');
+
+    closeModal.addEventListener('click', () => instance.close());
+
+    document.addEventListener('keydown', (event) => {
+        if (event.code === "Escape") {
+    instance.close();
+        };
+    });
+};
+
+function createMurkUpModal(card) {
+    let markUP;
+    const { thumb, title, rating, time, instructions, ingredients, tags, youtube, } = card;
+    const ingMark = ingredients.map(({ name, measure }) => `<li class="ing-item">
     <p class="ing-name">${name}</p>
     <p class="ing-measure">${measure}</p>
     </li>`).join('');
-        let tagMark = tags.map((tag) => `<li class="tags-item">
+    let tagMark = tags.map((tag) => `<li class="tags-item">
     <p class="tags-name">#${tag}</p>
-    </li>`);
-        if (tagMark.length > 3) {
-            tagMark.length = 3;
-        };
-        tagMark = tagMark.join('');
-        console.log(tagMark)
-       
+    </li>`).join('');
+    let ratingStar = createRatingStar(rating);
+    let youtubeLink = createYoutubeLink(youtube);
+    console.log(youtubeLink)
 
-
-        const instance = basicLightbox.create(`<div class="modal"><div class="img-title-box"><img src="${thumb}" alt="${title}" class="modal-img">
-
+     markUP = (`<div class="modal categories-block-modal"><div class="img-title-box"><iframe class="modal-img" width="295" height="295" src="https://www.youtube.com/embed/${youtubeLink}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     <h1 class="modal-title">${title}</h1></div>
+    <div class="desc-box">
     <div class="raiting-time-box">
-    <ul class="tags-list list">${tagMark}</ul>
-    <p class="raiting">${rating}</p>
+    <p class="raiting-text">${rating}</p>
+    <ul class="star-list list">${ratingStar}</ul>
     <p class="time">${time} min</p>
     </div>
+    <div class="ing-box">
     <ul class="ing-list list">${ingMark}</ul>
+    </div>
+    <div class="tags-box">
+    <ul class="tags-list list">${tagMark}</ul>
+    </div>
+    </div>
     <p class="instruction">${instructions}</p>
     <button type="button" class="modal-close-btn js-modal-close">
     <svg class="modal-close" width="20" height="20">
@@ -56,32 +90,62 @@ async function getCard(id) {
     </svg>
     </button>
     <div class="button-block">
-    <button class="btn add-to-favorite" type="button">Add to favorite</button>
+    <button class="btn add-to-favorite js-favorite-btn" type="button">Add to favorite</button>
     <button class="btn order-now js-rating-btn" type="button">Give a rating</button>
     </div>
     </div>`);
-        instance.show();
-        
-    const closeModal = document.querySelector('.js-modal-close');
-
-        closeModal.addEventListener('click', clickClose);
-        document.addEventListener('keydown', closeEscape);
-
-    function closeEscape(event) {
-       if (event.code === "Escape") {
-    instance.close();
-    document.removeEventListener('keydown', closeEscape);
-  }
-
-}
-    function clickClose(){
-        instance.close();
-        document.removeEventListener('click', clickClose);
-    }
-
-    } catch (error) {
-        console.log(error);
-    }
+    
+        return (markUP);
 };
 
+function createRatingStar(rating) {
+    const yellowStar = ` <li class="item-star-icon">
+        <svg width="18" height="18">
+            <use href="img/sprite/icons.svg#icon-star"></use>
+        </svg>
+    </li>`
+    const grayStar = `<li class="item-star-icon" >
+        <svg width="18" height="18">
+            <use href="img/sprite/icons.svg#icon-star-j"></use>
+        </svg>
+    </li>`
+    const newRating = Math.round(rating);
+    let arrStars = [];
+    for (let i = 0; i < 5; i++){
+        if (i + 1 > newRating) {
+            arrStars.push(grayStar);
+        } else {
+            arrStars.push(yellowStar)
+        }
+    }
+    return arrStars.join('');
+    
+}
+
+function createYoutubeLink(youtube) {
+    let link = youtube;
+    let idx = link.indexOf('=');
+    if (idx !== -1) {
+        let newLink = link.substring(idx + 1);
+        return newLink;
+    } else return;
+}
+
+
+function addFavorite(card) {
+    let existingData = localStorage.getItem("cardData");
+    let arrFavorite = existingData ? JSON.parse(existingData) : [];
+        const result = arrFavorite.find(({ _id }) => _id === card._id);
+        if (!result) {
+            arrFavorite.push(card);
+            
+        } else {
+            Notify.failure('This recipe has already been added to your favorites');
+            return
+    } 
+    let jsonData = JSON.stringify(arrFavorite)
+            localStorage.setItem("cardData", jsonData);
+            console.log(localStorage);
+
+}
 
