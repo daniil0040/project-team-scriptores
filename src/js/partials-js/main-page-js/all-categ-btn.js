cbaaa816a9ca57f250ae60b821ec24e46fe2ab39
AndrewOPP <a.vyblov2003@gmail.com>
2023-09-17 13:55:53 +0300

    categories done

--- src/js/partials-js/main-page-js/all-categ-btn.js
+++ src/js/partials-js/main-page-js/all-categ-btn.js
@@ -1,2 +1,24 @@
 import { createAllCategCardsMarkup } from "./all-categ-cards"
+import { isAllCategories } from "./categories"
+import { getAllRecipes } from "../service-api";
+import { cardsContainer } from './categories';
+export const buttonAllCategories = document.querySelector(".all-categories")
 
+
+buttonAllCategories.addEventListener("click", displayAllCategories)
+
+function displayAllCategories() {
+    if (isAllCategories || buttonAllCategories.classList.contains("all-categories-button-active")) {
+        return
+
+    } else {
+        getAllRecipes("").then(data => {
+            buttonAllCategories.classList.add("all-categories-button-active")
+            cardsContainer.innerHTML = createAllCategCardsMarkup(data.results)
+        })
+        .catch(err => {
+        console.log(err);
+        })
+    
+    }
+}
