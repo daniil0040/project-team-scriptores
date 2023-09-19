5a1d4b7361f131bc0c339dffc125c7fb36fdd46b
Serhii Haber <haberserhii@icloud.com>
2023-09-19 15:43:21 +0300

    Update switcher-theme.js

--- src/js/partials-js/main-page-js/switcher-theme.js
+++ src/js/partials-js/main-page-js/switcher-theme.js
@@ -1,31 +1,55 @@
-function setThemeFromLocalStorage() {
-  const storedTheme = localStorage.getItem('theme');
+const LIGHT_THEME = 'light';
+const DARK_THEME = 'dark';
+const ATTR_NAME = 'data-theme';
+const STORAGE_PROP_NAME = 'theme';
+
+const getTheme = () => {
+  const storedTheme = localStorage.getItem(STORAGE_PROP_NAME);
+
   const userPreferredTheme = window.matchMedia('(prefers-color-scheme: dark)')
     .matches
-    ? 'dark'
-    : 'light';
-  const currentTheme = storedTheme || userPreferredTheme;
-  document.documentElement.setAttribute('data-theme', currentTheme);
+    ? DARK_THEME
+    : LIGHT_THEME;
+  return storedTheme || userPreferredTheme;
+};
+
+const setThemeAttr = value => {
+  document.documentElement.setAttribute(ATTR_NAME, value);
+
   const toggles = document.querySelectorAll('.input-switcher');
-  toggles.forEach(function (toggle) {
-    toggle.checked = currentTheme === 'dark';
-  });
-}
 
-function toggleTheme() {
-  const currentTheme = document.documentElement.getAttribute('data-theme');
-  const targetTheme = currentTheme === 'light' ? 'dark' : 'light';
-  document.documentElement.setAttribute('data-theme', targetTheme);
+  if (!toggles.length) {
+    return;
+  }
+
+  toggles.forEach(toggle => {
+    toggle.checked = value === DARK_THEME;
+  });
+};
+
+const toggleTheme = () => {
+  const currentTheme = getTheme();
+
+  const targetTheme = currentTheme === LIGHT_THEME ? DARK_THEME : LIGHT_THEME;
+
+  setThemeAttr(targetTheme);
+
+  localStorage.setItem(STORAGE_PROP_NAME, targetTheme);
+};
+
+const addToggleEventListeners = () => {
   const toggles = document.querySelectorAll('.input-switcher');
-  toggles.forEach(function (toggle) {
-    toggle.checked = targetTheme === 'dark';
+  toggles.forEach(toggle => {
+    toggle.addEventListener('change', toggleTheme);
   });
-  localStorage.setItem('theme', targetTheme);
-}
+};
 
-const toggles = document.querySelectorAll('.input-switcher');
-toggles.forEach(function (toggle) {
-  toggle.addEventListener('change', toggleTheme);
-});
+const initializeTheme = () => {
+  const currentTheme = getTheme();
 
-setThemeFromLocalStorage();
+  setThemeAttr(currentTheme);
+
+  addToggleEventListeners();
+};
+
+initializeTheme();
