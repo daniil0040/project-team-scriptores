bdc24aa1886c238c21d71ac5e1ffa26325f3484c
Paul Derkach <mr.derkach.55@gmail.com>
2023-09-20 19:31:15 +0300

    in progress
    Змінив кольори до модалки через змінні. Нажаль, так і не виходить реалізувати функцію щоб дані відправлялись на бек

--- src/js/partials-js/main-page-js/order-now.js
+++ src/js/partials-js/main-page-js/order-now.js
@@ -1,10 +1,14 @@
-	const openModalButton = document.getElementById('openModalBtn');
-	const closeModalButton = document.getElementById('closeModalBtn');
-	const modal = document.getElementById('modal');
-	const overlay = document.getElementById('overlay');
-	const orderForm = document.getElementById('orderForm');
+import axios from 'axios';
+import Notiflix from 'notiflix';
 
-// Функція відериття модального вікна
+const openModalButton = document.getElementById('openModalBtn');
+const closeModalButton = document.getElementById('closeModalBtn');
+const modal = document.getElementById('modal');
+const overlay = document.getElementById('overlay');
+const orderForm = document.getElementById('orderForm');
+const BASE_URL = 'https://tasty-treats-backend.p.goit.global/api';
+
+// Функція відкриття модального вікна
 openModalBtn.addEventListener('click', () => {
     overlay.style.display = 'block';
     orderModal.style.display = 'block';
@@ -44,4 +48,44 @@ orderModal.addEventListener('click', (e) => {
 // Заборонити прокрутку в модальному вікні
 orderModal.addEventListener('scroll', (e) => {
     e.preventDefault();
+});
+
+// Функція відправки даних на бекенд і обробки відповіді
+function sendRequest(data) {
+    const url = `${BASE_URL}/orders/add`; 
+
+    axios.post(url, data, {
+        headers: {
+            'Content-Type': 'application/json', 
+        },
+    })
+        .then(response => {
+            if (response.data.success) {
+                // Закриваємо модальне вікно
+                closeModalBtn.click();
+                // Відображаємо нотіфікацію про успішну відправку
+                Notiflix.Notify.success('Дані успішно відправлені на сервер');
+            } else {
+                // Відображаємо помилку користувачу
+                Notiflix.Notify.failure('Помилка: ' + response.data.error);
+            }
+        })
+        .catch(error => {
+            console.error('Помилка при відправленні запиту:', error);
+            // Відображаємо помилку користувачу
+            Notiflix.Notify.failure('Помилка при відправленні запиту.');
+        });
+}
+
+// Очищення полів форми при натисканні кнопки "Send"
+orderForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = {
+        name: document.getElementById('name').value,
+        phone: document.getElementById('phone').value,
+        email: document.getElementById('email').value,
+        comment: document.getElementById('comment').value,
+    };
+    sendRequest(formData); // Відправляємо дані на бекенд
+    orderForm.reset();
 });
